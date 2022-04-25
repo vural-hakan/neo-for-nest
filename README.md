29fcbe359eb420d70f142ff6564ac3ff5223f97d
Hakan Vural <hakanvural.dev@gmail.com>
2022-04-25 09:24:03 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,82 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://kamilmysliwiec.com/public/nest-logo.png#1" alt="Nest Logo" />   </a>
+  <a href="https://neo4j.com" target="_blank"><img src="https://dist.neo4j.com/wp-content/uploads/20140926224303/neo4j_logo-facebook.png" width="380"></a>
+</p>
+
+# Nest Neo4j
+
+> Neo4j integration for Nest.js
+
+## Description
+
+This repository provides [Neo4j](https://www.neo4j.com) integration for [Nest](http://nestjs.com/).
+
+## Installation
+
+```
+$ npm i --save neo-for-nest
+```
+
+## Quick Start
+
+Register the Neo4j Module in your application using the `forRoot` method, passing the neo4j connection information as an object:
+
+```ts
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { Neo4jModule } from 'nest-neo4j'
+
+@Module({
+  imports: [
+    Neo4jModule.forRoot({
+      scheme: 'neo4j',
+      host: 'localhost',
+      port: 7687,
+      username: 'neo4j',
+      password: 'neo4j'
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
+```
+
+## Querying Neo4j
+
+The `neo-for-nest` is `@Injectable`, so can be passed into any constructor:
+
+```ts
+import { Neo4jService } from 'neo-for-nest'
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService,
+    private readonly neo4jService: Neo4jService
+    ) {}
+
+  @Get()
+  async getHello(): Promise<any> {
+    const res = await this.neo4jService.read(`MATCH (n) RETURN count(n) AS count`)
+
+    return `There are ${res.records[0].get('count')} nodes in the database`
+  }
+}
+```
+
+## Methods
+
+```ts
+getConfig(): Neo4jConfig;
+getReadSession(database?: string): Session;
+getWriteSession(database?: string): Session;
+read(query: string, params?: object, database?: string): Result;
+write(query: string, params?: object, database?: string): Result;
+```
+
+## Thanks
+
+```
+https://github.com/adam-cowley/nest-neo4j
+```
---
+++ src/neo4j.module.ts
@@ -0,0 +1,80 @@
+import { Module, DynamicModule, Provider } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Neo4jService } from './neo4j.service';
+import { Neo4jConfig } from './interfaces/neo4j-config.interface';
+import { NEO4J_OPTIONS, NEO4J_DRIVER } from './neo4j.constants';
+import { createDriver } from './neo4j.utils';
+
+@Module({})
+export class Neo4jModule {
+  static forRoot(config: Neo4jConfig): DynamicModule {
+    return {
+      module: Neo4jModule,
+      global: true,
+      providers: [
+        {
+          provide: NEO4J_OPTIONS,
+          useValue: config,
+        },
+        {
+          provide: NEO4J_DRIVER,
+          inject: [NEO4J_OPTIONS],
+          useFactory: async (config: Neo4jConfig) => createDriver(config),
+        },
+        Neo4jService,
+      ],
+      exports: [Neo4jService],
+    };
+  }
+
+  static forRootAsync(configProvider: any): DynamicModule {
+    return {
+      module: Neo4jModule,
+      global: true,
+      imports: [ConfigModule],
+
+      providers: [
+        {
+          provide: NEO4J_OPTIONS,
+          ...configProvider,
+        } as Provider<any>,
+        {
+          provide: NEO4J_DRIVER,
+          inject: [NEO4J_OPTIONS],
+          useFactory: async (config: Neo4jConfig) => createDriver(config),
+        },
+        Neo4jService,
+      ],
+      exports: [Neo4jService],
+    };
+  }
+
+  static fromEnv(): DynamicModule {
+    return {
+      module: Neo4jModule,
+      global: true,
+      imports: [ConfigModule],
+      providers: [
+        {
+          provide: NEO4J_OPTIONS,
+          inject: [ConfigService],
+          useFactory: (configService: ConfigService): Neo4jConfig => ({
+            scheme: configService.get('NEO4J_SCHEME'),
+            host: configService.get('NEO4J_HOST'),
+            port: configService.get('NEO4J_PORT'),
+            username: configService.get('NEO4J_USERNAME'),
+            password: configService.get('NEO4J_PASSWORD'),
+            database: configService.get('NEO4J_DATABASE'),
+          }),
+        } as Provider<any>,
+        {
+          provide: NEO4J_DRIVER,
+          inject: [NEO4J_OPTIONS],
+          useFactory: async (config: Neo4jConfig) => createDriver(config),
+        },
+        Neo4jService,
+      ],
+      exports: [Neo4jService],
+    };
+  }
+}
